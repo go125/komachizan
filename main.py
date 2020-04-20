054b36ab3dd4d1a4faf42cd6c7a24eaa1e54da8b
go125 <34290064+go125@users.noreply.github.com>
2020-04-20 12:39:36 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import itertools
+
+def make_oplist(num):
+    op_list=[]
+    for i in range(0,9):
+        rand_int=num%4
+        if rand_int==0:
+            op_list.append("+")
+        if rand_int==1:
+            op_list.append("-")
+        if rand_int==2:
+            op_list.append("*")
+        if rand_int==3:
+            op_list.append("/")
+        num=num//4
+    return op_list
+
+def func(seqs,oplist):
+    tmp_list=["1","2","3","4","5","6","7","8","9"]
+    tmp_last_list=[1,2,3,4,5,6,7,8,9]
+    #end number of each content of tmp_list
+    for j in range (0,len(seqs)):
+        last_num=seqs[j]+1
+        next_first_num=last_num+1
+        for k in range(0,len(tmp_last_list)):
+            if tmp_last_list[k]==next_first_num:
+                last_num_list_num=k-1
+        new_tmp_list=[]
+        new_tmp_last_list=[]
+        for m in range(0,last_num_list_num):
+            new_tmp_list.append(tmp_list[m])
+            new_tmp_last_list.append(tmp_last_list[m])
+        new_content="("+tmp_list[last_num_list_num]+oplist[last_num_list_num]+tmp_list[last_num_list_num+1]+")"
+        new_tmp_list.append(new_content)
+        new_tmp_last_list.append(tmp_last_list[last_num_list_num])
+
+        for m in range(last_num_list_num+2,len(tmp_list)):
+            new_tmp_list.append(tmp_list[m])
+            new_tmp_last_list.append(tmp_last_list[m])   
+
+        tmp_list=new_tmp_list
+        tmp_last_list=new_tmp_last_list
+ 
+
+    return tmp_list
+                
+good_result=[]
+
+def main(max_count=4**8,margin=0.00001):
+    global good_result, seq_list
+    count=0
+    while count<=max_count:
+        oplist=make_oplist(count)
+        for i in range(0,len(seq_list)):
+            if i%10000==0:
+                print("In " +str(count)+" step, "+str(i)+"/"+str(len(seq_list))+" has finished.")
+                print(func(seq_list[i],oplist))
+            try:
+                eval(func(seq_list[i],oplist))
+            except SyntaxError:
+                count+=1
+                continue
+            except ZeroDivisionError:
+                count+=1
+                continue
+            res=eval(func(seq_list[i],oplist))
+            error=res-100
+            if abs(error)<margin:
+                print(func(seq_list[i],oplist))
+                good_result.append(func(seq_list[i],oplist))
+        count+=1
+
+main()
+
+print(good_result)
+
+len(good_result)
